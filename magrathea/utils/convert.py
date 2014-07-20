a0b5c13daea47c9fb5cd26ab95cd23f02bea75cd
Daemotron <mail@daemotron.net>
2014-07-20 12:54:22 +0200

    Fixed Python 2.7 compatibility for magrathea.utils.convert

--- magrathea/utils/convert.py
+++ magrathea/utils/convert.py
@@ -6,6 +6,7 @@
     :copyright: Copyright 2014 by the RootForum.org team, see AUTHORS.
     :license: MIT License, see LICENSE for details.
 """
+import sys
 from ..conf import get_conf
 
 
@@ -16,30 +17,50 @@ def to_bytes(value):
     :param value: The value to be converted
     :return: Byte sequence
     """
-    if isinstance(value, str):
-        # noinspection PyArgumentList
-        return bytes(source=value, encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
-    elif isinstance(value, bytearray):
-        return bytes(value)
-    elif isinstance(value, bytes):
-        return value
-    elif value is None:
-        return b''
+    if sys.version_info < (3, 0, 0):
+        if isinstance(value, unicode):
+            return str(bytearray(source=value, encoding=get_conf('DEFAULT_CHARSET'), errors='replace'))
+        elif isinstance(value, bytearray):
+            return str(value)
+        elif value is None:
+            return b''
+        else:
+            return str(bytearray(source=str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace'))
     else:
-        # noinspection PyArgumentList
-        return bytes(source=str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
+        if isinstance(value, str):
+            # noinspection PyArgumentList
+            return bytes(source=value, encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
+        elif isinstance(value, bytearray):
+            return bytes(value)
+        elif isinstance(value, bytes):
+            return value
+        elif value is None:
+            return b''
+        else:
+            # noinspection PyArgumentList
+            return bytes(source=str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
 
 
 def to_str(value):
     """
-    Convert any value into a string.
+    Convert any value into a unicode string.
 
     :param value: The value to be converted
     :return: Resulting string
     """
-    if isinstance(value, bytes) or isinstance(value, bytearray):
-        return value.decode(encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
-    elif value is None:
-        return ''
+    if sys.version_info < (3, 0, 0):
+        if isinstance(value, str):
+            return unicode(value, encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
+        elif isinstance(value, bytearray):
+            return unicode(str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
+        elif value is None:
+            return u''
+        else:
+            return unicode(str(value), encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
     else:
-        return str(value)
+        if isinstance(value, bytes) or isinstance(value, bytearray):
+            return value.decode(encoding=get_conf('DEFAULT_CHARSET'), errors='replace')
+        elif value is None:
+            return ''
+        else:
+            return str(value)
--- test/t_utils/test_convert.py
+++ test/t_utils/test_convert.py
@@ -6,7 +6,8 @@
     :copyright: Copyright 2014 by the RootForum.org team, see AUTHORS.
     :license: MIT License, see LICENSE for details.
 """
-from unittest import TestCase
+from unittest import TestCase, skipIf
+import sys
 from magrathea.utils.convert import to_bytes, to_str
 
 
@@ -99,6 +100,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_bytes(test_string), bytes)
         self.assertSequenceEqual(to_bytes(test_string), test_bytes)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_08(self):
         """
         Test Case 08:
@@ -111,6 +113,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_09(self):
         """
         Test Case 09:
@@ -123,6 +126,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_10(self):
         """
         Test Case 10:
@@ -135,6 +139,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_11(self):
         """
         Test Case 11:
@@ -147,6 +152,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_12(self):
         """
         Test Case 12:
@@ -159,6 +165,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_13(self):
         """
         Test Case 13:
@@ -171,6 +178,7 @@ class TestConvert(TestCase):
         self.assertIsInstance(to_str(test_bytes), str)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
 
+    @skipIf(sys.version_info < (3, 0, 0), 'Test only for Python 3')
     def test_14(self):
         """
         Test Case 14:
@@ -181,4 +189,95 @@ class TestConvert(TestCase):
         test_string = "[1, 2, 3]"
         test_bytes = [1, 2, 3]
         self.assertIsInstance(to_str(test_bytes), str)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_15(self):
+        """
+        Test Case 15:
+        Convert a non-empty byte sequence into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u"test"
+        test_bytes = b"test"
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_16(self):
+        """
+        Test Case 16:
+        Convert an empty byte sequence into a string.
+
+        Test is passed if the returned result is a sting and equals the expected result.
+        """
+        test_string = u""
+        test_bytes = b""
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_17(self):
+        """
+        Test Case 17:
+        Convert a byte sequence into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u"test"
+        test_bytes = bytes(b"test")
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_18(self):
+        """
+        Test Case 18:
+        Convert a byte array into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u"test"
+        test_bytes = bytearray(b"test")
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_19(self):
+        """
+        Test Case 19:
+        Convert None into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u""
+        test_bytes = None
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_20(self):
+        """
+        Test Case 20:
+        Convert False into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u"False"
+        test_bytes = False
+        self.assertIsInstance(to_str(test_bytes), unicode)
+        self.assertSequenceEqual(to_str(test_bytes), test_string)
+
+    @skipIf(sys.version_info >= (3, 0, 0), 'Test only for Python 2.7')
+    def test_21(self):
+        """
+        Test Case 21:
+        Convert a non-bytes object into a string.
+
+        Test is passed if the returned result is a string and equals the expected result.
+        """
+        test_string = u"[1, 2, 3]"
+        test_bytes = [1, 2, 3]
+        self.assertIsInstance(to_str(test_bytes), unicode)
         self.assertSequenceEqual(to_str(test_bytes), test_string)
