9a522e90da9ee92842b757d39520673f0261fb84
daemotron <mail@daemotron.net>
2014-07-27 15:44:16 +0200

    Started unit test module for utils.version

---
+++ test/t_utils/test_version.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    test.t_utils.test_version
+    ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    :copyright: Copyright 2014 by the RootForum.org team, see AUTHORS.
+    :license: MIT License, see LICENSE for details.
+"""
+from unittest import TestCase
+
+
+class TestMagratheaUtilsVersion(TestCase):
+    """
+    Unit tests for :py:mod:`magrathea.utils.version`
+    """
+
+    pass
