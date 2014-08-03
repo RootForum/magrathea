92aa144b182e7e69956c6a47b2e1d0508c1be077
daemotron <mail@daemotron.net>
2014-08-03 23:42:40 +0200

    Enhanced inline documentation for magrathea.cli.execute

--- magrathea/cli/__init__.py
+++ magrathea/cli/__init__.py
@@ -13,6 +13,14 @@ def execute(argv=None):
     """
     Function being called from the executable to launch the CLI.
     This is the initial entrance point for any Magrathea processing.
+    Usually, this function is invoked by an executable Python script
+    to start the actual Magrathea CLI process. This could look like the
+    following example::
+
+       import sys
+       from magrathea.cli import execute
+
+       sys.exit(execute(sys.argv))
 
     :param list argv: list of (command line) arguments
     """
